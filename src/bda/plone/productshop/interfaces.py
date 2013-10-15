10e5a350fdd6696b39b1c30c703ec432f265dd9c
Espen Moe-Nilssen <espen@medialog.no>
2013-10-15 15:16:16 +0200

    first commit

---
+++ src/bda/plone/productshop/interfaces.py
@@ -0,0 +1,71 @@
+from zope.interface import Interface
+from plone.namedfile.interfaces import IImageScaleTraversable
+from collective.instancebehavior import IInstanceBehaviorAssignableContent
+
+#added by espen
+#not sure if all is needed
+#from plone.registry.interfaces import IRegistry
+from zope.i18nmessageid import MessageFactory
+from z3c.form import interfaces
+from zope import schema
+from zope.interface import alsoProvides
+from plone.directives import form
+from bda.plone.shop.interfaces import IShopSettingsProvider
+
+_ = MessageFactory('bda.plone.productshop')
+
+
+class IProductShopExtensionLayer(Interface):
+    """Product shop specific browser layer.
+    """
+
+
+##############################################################################
+# content markers
+##############################################################################
+
+class IProduct(Interface, IImageScaleTraversable):
+    """Marker interface for product content.
+    """
+
+
+class IProductGroup(IProduct):
+    """Marker interface for product group content.
+    """
+
+
+class IVariant(IProduct, IInstanceBehaviorAssignableContent):
+    """Marker interface for variant content.
+    """
+
+
+class IVariantAspect(Interface):
+    """Aspect of a variant.
+    """
+
+
+                                                         
+      
+##############################################################################
+# control panel (tab)
+##############################################################################
+                            
+class IShopTabsSettings(form.Schema):
+    """Adds settings to bda.plone.shop's controlpanel
+    """
+
+    form.fieldset(
+        'tabs',
+        label=_(u'TabsSettings'),
+        fields=[
+            'rtf_fields',
+            ],
+        )
+
+    rtf_fields = schema.Set(
+                        title=_(u"Render which (rtf) fields as Tabs"),
+                        value_type=schema.Choice(values=[('Datasheet'), ('Details')])
+                        )
+
+
+alsoProvides(IShopTabsSettings, IShopSettingsProvider)
